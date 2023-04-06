59c45e435c5330433cd2fdb0371793ce4cfc5bf4
Pedro Specter <pedrospecter@Pedros-MacBook-Pro.local>
2023-04-06 17:53:51 +0200

    dd

--- components/footer.jsx
+++ components/footer.jsx
@@ -10,14 +10,18 @@ export default function Footer() {
 
     return (
         <div className="bg-dark text-white py-3">
-            <div className="container-fluid  pb-4">
+            <div className="container  pb-4">
                 <Image src={"/img/logo/logo_full_white.svg"} alt="cw1" width={350} height={100}></Image>
+                <div>
+                    <img src="/img/logo/appstore.svg" alt="Sexy Bark | cw1" width="170px" style={{ objectFit: "contain", maxHeight: 100, cursor: "pointer" }} />
+                    <img src="/img/logo/playstore.svg" alt="Sexy Bark | cw1" width="200px" style={{ objectFit: "contain", maxHeight: 100, marginLeft: 30, cursor: "pointer" }} />
+                </div>
             </div>
             <div className="container-fluid px-md-5 d-flex justify-content-between align-items-center border-top pt-3" style={{ fontSize: 12 }}>
                 <span>@ 2023 <a href="https://www.cw1.com"> CW1 Inc.</a> All rights reserved</span>
                 <div className="d-flex justify-content-center">
-                    <span className="pointer px-3" onClick={() => router.push("/legal")}>Legal</span>
-                    <span className="pointer ps-3 border-start" onClick={() => router.push("/contact")}>Contact</span>
+                    <span className="pointer px-3" onClick={() => router.push("/legal/privacy-policy")}>Privacy Policy</span>
+                    {/* <span className="pointer ps-3 border-start" onClick={() => router.push("/contact")}>Contact</span> */}
                 </div>
 
             </div>
---
+++ pages/legal/privacy-policy.jsx
@@ -0,0 +1,179 @@
+import Footer from '@/components/footer'
+import Header from '@/components/header'
+import React from 'react'
+
+
+
+
+export default function () {
+
+
+    return (
+        <>
+            <Header backgroundColor='#444' />
+            <div className="container py-5 privacy-policy">
+
+                <div>
+                    <h3>Privacy Policy</h3>
+                    <p>CW1 | 06.04.2023</p>
+
+                    <p>Thank you for visiting the Privacy Policy of SexyBark - a product provided by CW1 Inc. This Privacy Policy explains how CW1 (collectively, “CW1”, “we”, “us”, or “our”) collects, uses, and shares information about you (“you”, “yours” or “user”) when you access or use our websites (“Services”).</p>
+                    <p>Users are responsible for any third-party data they provide or share through the Services and confirm that they have the third-party's consent to provide such data to us.</p>
+                    <br />
+
+                    <h3>Information We Collect</h3>
+
+                    <p><strong>Log and Usage Information: </strong>such as browser type you use, hardware model, operating system, IP address, unique device identifiers, access times, pages viewed, links clicked, and browsing behavior such as time spent, what site you came from, what site you visit when you leave us, and browsing behavior.</p>
+                    <p><strong>Account Information: </strong>if you create an account on the Services, we collect the information you provide to us related to the account, such as first and last name, username, and mobile phone, and activation tokens. </p>
+                    <p><strong>Transactional Information: </strong>such as items placed in your cart, products purchased, product details,  purchase price, purchased date and payment information.</p>
+                    <br />
+
+
+                    <h3>How We Use Your Information</h3>
+                    <p>We use information we collect about you to provide, maintain, and improve our Services and other interactions we have with you. For example, we use the information collected to:</p>
+                    <ul>
+                        <li>
+                            <p>Facilitate and improve our online experience;</p>
+                        </li>
+                        <li>
+                            <p>Provide and deliver products and services, perform authentication, process transactions and returns, and send you related information, including confirmations, receipts, invoices, customer experience surveys, and product or Services-related notices;</p>
+                        </li>
+                        <li>
+                            <p>Process and deliver promotions;</p>
+                        </li>
+                        <li>
+                            <p>Respond to your comments and questions and provide customer service;</p>
+                        </li>
+                        <li>
+                            <p>If you have indicated to us that you wish to receive notifications or promotional messages;</p>
+                        </li>
+                        <li>
+                            <p>Detect, investigate and prevent fraudulent transactions and other illegal activities and protect our rights and property and others;</p>
+                        </li>
+                        <li>
+                            <p>Comply with our legal and financial obligations;</p>
+                        </li>
+                        <li>
+                            <p>Monitor and analyze trends, usage, and activities;</p>
+                        </li>
+                        <li>
+                            <p>Provide and allow our partners to provide advertising and marketing targeted toward your interests.</p>
+                        </li></ul>
+                    <br />
+                    <h3>How We May Share Information</h3>
+                    <p>We may share your Personal Information in the following situations:</p>
+                    <ul>
+                        <li>
+                            <p><strong>With Microsoft Azure. </strong>We may share data with Microsoft Azure since our datacenter is hosted in the cloud and Apple Inc or Google Inc when handling payment information.</p>
+                        </li>
+                        <li>
+                            <p><strong>Change in Business. </strong>We may share data in connection with a corporate business transaction, such as a merger or acquisition of all or a portion of our business to another company, joint venture, corporate reorganization, insolvency or bankruptcy, financing or sale of company assets;</p>
+                        </li>
+                        <li>
+                            <p><strong>To Comply with Law. </strong>We may share data to facilitate legal process from lawful requests by public authorities, including to meet national security or law enforcement demands as permitted by law.</p>
+                        </li>
+                        <li>
+                            <p><strong>With Your Consent. </strong>We may share data with third parties when we have your consent.</p>
+                        </li>
+                        <li>
+                            <p><strong>With Advertising and Analytics Partners. </strong>See the section entitled “Advertising and Analytics” below.</p>
+                        </li>
+                    </ul>
+                    <br />
+
+
+                    <h3>Ads on the Service</h3>
+                    <p>We may use third-party advertising companies to serve content and advertisements when you visit our App.</p>
+                    <br />
+                    <h3>Data Security</h3>
+                    <p>We implement commercially reasonable security measures designed to protect your information. Despite our best efforts, however, no security measures are completely impenetrable.</p>
+                    <br />
+                    <h3>Data Retention</h3>
+                    <p>We store the information we collect about you for as long as necessary for the purpose(s) for which we collected it or for other legitimate business purposes, including to meet our legal, regulatory, or other compliance obligations.</p>
+                    <br />
+                    <h3>California Privacy Rights</h3>
+
+                    <p>If you are a California resident and the processing of personal information about you is subject to the California Consumer Privacy Act (“CCPA”), you have certain rights with respect to that personal information. Under the CCPA, subject to certain exceptions, “personal information” is any information that identifies, relates to, describes, is reasonably capable of being associated with, or could reasonably be linked, directly or indirectly, with a particular California resident or household.</p>
+                    <p>If you are a CA resident, you have a right to request that we provide you with the following information:</p>
+                    <ul>
+                        <li>
+                            <p>The categories and specific pieces of personal information we have collected about you. We have disclosed the categories of information under “Information We Collect”, above.</p>
+                        </li>
+                        <li>
+                            <p>The categories of sources from which we collect personal information. We have disclosed the categories of sources under “Source of Information and Tracking Technologies”, above.</p>
+                        </li>
+                        <li>
+                            <p>The purposes for collecting, using, or selling personal information. We have disclosed these purposes under “How We Use Your Information” and “How We May Share Information” above.</p>
+                        </li>
+                        <li>
+                            <p>The categories of third parties with which we share personal information. We have disclosed the categories of third parties under “How We May Share Information” above.</p>
+                        </li>
+                        <li>
+                            <p>The categories of personal information disclosed about you for a business purpose. We have disclosed the categories of third parties under “How We May Share Information” above.</p>
+                        </li></ul>
+
+                    <p>You also have a right to request that we delete your CCPA personal information under certain circumstances, subject to certain exceptions.</p>
+                    <p>Finally, you have a right not to be discriminated against for exercising rights set out in the CCPA.</p>
+                    <p>Please note we have the right to take reasonable steps to verify your identity and the authenticity of your request.</p>
+                    <br />
+                    <h3>EU Privacy Rights</h3>
+
+                    <p>Individuals located in certain countries, including the European Economic Area (EEA) and the United Kingdom, have certain statutory rights under the General Data Protection Regulation (GDPR) in relation to their personal data.</p>
+                    <p>To the extent information we collect is associated with an identified or identifiable natural person and is protected as personal data under GDPR, it is referred to in this Privacy Policy as “Personal Data”.</p>
+                    <p><strong>Data Subject Access Requests</strong></p>
+                    <p>Subject to any exemptions provided by law, you may have the right to request:</p>
+                    <ul>
+                        <li>
+                            <p>a copy of the Personal Data we hold about you;</p>
+                        </li>
+                        <li>
+                            <p>to correct the Personal Data we hold about you;</p>
+                        </li>
+                        <li>
+                            <p>to delete your Account or Personal Data;</p>
+                        </li>
+                        <li>
+                            <p>to object to processing of your Personal Data for certain purposes;</p>
+                        </li></ul>
+
+                    <p>To access your privacy rights, send us an email at info@cw1.com.</p>
+                    <p>We will generally process requests within one month. We may need to request specific information from you to help us confirm your identity and/or the jurisdiction in which you reside. If your request is complicated or if you have made a large number of requests, it may take us longer. We will let you know if we need longer than one month to respond.</p>
+                    <p><strong>Legal Bases For Processing Personal Data</strong></p>
+                    <p>We may process your Personal Data under applicable data protection law on the following legal grounds:</p>
+                    <ul>
+                        <li>
+                            <p><strong>Contractual Necessity: </strong>we may process your Personal Data to enter into or perform a contract with you.</p>
+                        </li>
+                        <li>
+                            <p><strong>Consent: </strong>where you have provided consent to process your Personal Data. You may withdraw your consent at any time.</p>
+                        </li>
+                        <li>
+                            <p><strong>Legitimate interest: </strong>we process your Personal Data to provide our Services to you such as to provide our online user experience, communicate with you, provide customer service, market, analyze and improve our business, and to protect our Services.</p>
+                        </li></ul>
+                    <br />
+                    <h3>Age Limitations</h3>
+                    <p>Our Service is intended for adults ages 18 years and above or young teenagers 13+.   We do not knowingly collect personally identifiable information from children. If you are a parent or legal guardian and think your child under 13 has given us information, please email or write to us at the address listed at the end of this Privacy Policy. Please mark your inquiries “COPPA Information Request.”</p>
+                    <br />
+                    <h3>Changes to this Privacy Policy</h3>
+                    <p>CW1 may change this Privacy Policy from time to time. We encourage you to visit this page to stay informed. If the changes are material, we may provide you additional notice to your email address or through our Services. Your continued use of the Services indicates your acceptance of the modified Privacy Policy.</p>
+                    <br />
+
+                    <h3>Storage of Information in the United States</h3>
+                    <p>Information we maintain may be stored both within and outside of the United States. If you live outside of the United States, you understand and agree that we may transfer your information to the United States, and that U.S. laws may not afford the same level of protection as those in your country.</p>
+                    <br />
+                    <h3>Contact Us</h3>
+                    <p>If you have questions, comments, or concerns about this Privacy Policy, you may contact us at:</p>
+                    <p>The data controller responsible in accordance with the purposes of the General Data Protection Regulation (GDPR) and other data protection regulations is:</p>
+                    <p>CW1 AB <br />Torggatan 8 <br /> 411 05 Gothenburg <br />Sweden</p>
+                    <p className="text-primary">
+                        +46 (0)31 126 413 <br />
+                        info@cw1.com <br />
+                        www.cw1.com
+                    </p>
+
+                </div>
+            </div>
+            <Footer />
+        </>
+    )
+}
