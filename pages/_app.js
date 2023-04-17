7cbf688b1117d106b275eddf28213eb5013f033d
Pedro Specter <pedro.specter@cw1.com>
2023-04-17 10:58:40 +0200

    dd

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '@/styles/globals.scss'
 
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -10,6 +11,17 @@ const inter = Inter({ subsets: ['latin'] })
 export default function App({ Component, pageProps }) {
   return (
     <main className={inter.className}>
+      <Script
+        id="clarit"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        (function(c,l,a,r,i,t,y){
+          c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "gpvml0rpoi"); `}} />
+
       <Component {...pageProps} />
     </main>
   )
