258b52f0764d087080b7e4cd16292a439fa64447
Pedro Specter <pedro.specter@cw1.com>
2023-04-21 13:32:19 +0200

    dd

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '@/styles/globals.scss'
 
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 import Script from 'next/script'
 
 
@@ -11,6 +12,40 @@ const inter = Inter({ subsets: ['latin'] })
 export default function App({ Component, pageProps }) {
   return (
     <main className={inter.className}>
+      <Head>
+        <title>Sexy Bark</title>
+        <meta name="title" content={"Sexy Bark"} />
+        <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=9" />
+        <meta httpEquiv="Content-language" content="en" />
+        <meta charSet="utf-8" />
+        <meta name="description" content={"We connect dog lovers to find friends for their furry friends"} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href={`https://wwww.sexybark.com`} />
+
+        <meta property="og:type" content="website" />
+        <meta name="og:title" property="og:title" content={"Sexy Bark"} />
+        <meta name="og:description" property="og:description" content={"We connect dog lovers to find friends for their furry friends"} />
+        <meta property="og:site_name" content="CW1" />
+        <meta property="og:url" content={`https://wwww.sexybark.com`} />
+        <meta property="og:image" content={`https://i.ibb.co/64dCSRN/pspecter-selfies-of-attractive-swedish-girls-and-their-dogs-06509181-f258-4453-a9a5-f7168879750a.png`} />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content={"Sexy Bark"} />
+        <meta name="twitter:description" content={"We connect dog lovers to find friends for their furry friends"} />
+        <meta name="twitter:site" content={`https://wwww.sexybark.com`} />
+        <meta name="twitter:creator" content="CW1" />
+        <meta name="twitter:image" content={`https://i.ibb.co/64dCSRN/pspecter-selfies-of-attractive-swedish-girls-and-their-dogs-06509181-f258-4453-a9a5-f7168879750a.png`} />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#ffc40d" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <Script
         id="clarit"
         strategy="afterInteractive"
--- pages/index.jsx
+++ pages/index.jsx
@@ -8,40 +8,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Sexy Bark</title>
-        <meta name="title" content={"Sexy Bark"} />
-        <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=9" />
-        <meta httpEquiv="Content-language" content="en" />
-        <meta charSet="utf-8" />
-        <meta name="description" content={"We connect dog lovers to find friends for their furry friends"} />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="robots" content="index, follow" />
-        <link rel="canonical" href={`https://wwww.sexybark.com`} />
-
-        <meta property="og:type" content="website" />
-        <meta name="og:title" property="og:title" content={"Sexy Bark"} />
-        <meta name="og:description" property="og:description" content={"We connect dog lovers to find friends for their furry friends"} />
-        <meta property="og:site_name" content="CW1" />
-        <meta property="og:url" content={`https://wwww.sexybark.com`} />
-        <meta property="og:image" content={`https://i.ibb.co/64dCSRN/pspecter-selfies-of-attractive-swedish-girls-and-their-dogs-06509181-f258-4453-a9a5-f7168879750a.png`} />
-
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content={"Sexy Bark"} />
-        <meta name="twitter:description" content={"We connect dog lovers to find friends for their furry friends"} />
-        <meta name="twitter:site" content={`https://wwww.sexybark.com`} />
-        <meta name="twitter:creator" content="CW1" />
-        <meta name="twitter:image" content={`https://i.ibb.co/64dCSRN/pspecter-selfies-of-attractive-swedish-girls-and-their-dogs-06509181-f258-4453-a9a5-f7168879750a.png`} />
-
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#ffc40d" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
+  
       <main className={""}>
 
         <section className="hero">
